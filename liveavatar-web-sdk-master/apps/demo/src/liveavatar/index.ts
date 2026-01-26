ec0fd8616fade6b6d2d5fd8d4268da5f3615e7c7
Pritam-Routh <pritamrouth67123@gmail.com>
2026-01-26 21:04:18 +0530

    Remove deprecated classes and tests related to VideoGrant, WebhookReceiver, OkHttpSupplier, TransformCall, and various service clients in the server SDK. This cleanup enhances code maintainability and reduces unnecessary complexity.

--- liveavatar-web-sdk-master/apps/demo/src/liveavatar/index.ts
+++
@@ -1,6 +0,0 @@
-export { LiveAvatarContextProvider } from "./context";
-export * from "./types";
-export * from "./useChatHistory";
-export * from "./useSession";
-export * from "./useVoiceChat";
-export * from "./useTextChat";
